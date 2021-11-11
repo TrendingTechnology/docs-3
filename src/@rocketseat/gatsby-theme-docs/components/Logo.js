496a30e87717081040af952d17552c1645f2b6c2
Joel E. Rego <joelerego@outlook.com>
2021-11-11 10:17:49 +0530

    feat: minor changes to the ui and theme

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import logo from '../../../../static/logo.png';
+
+const Logo = () => (
+  <img src={logo} alt="Bothell.js" style={{ width: 180 }} />
+);
+
+export default Logo;
